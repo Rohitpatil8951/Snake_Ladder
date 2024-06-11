fed61608fb14279c04a99288bfc0253366ac52be
Rohitpatil8951 <rohitpatil8951@gmail.com>
2024-06-11 10:17:31 +0530

    Welcome to Snake_Ladder

---
+++ Snake_Ladder/MainCode.cs
@@ -0,0 +1,15 @@
+﻿namespace SnakeAndLadder
+{
+    class MainCode
+    {
+        static void Main(string[] args)
+        {
+            int playerPosition = 0;
+
+            Console.WriteLine("Welcome to the Snake and Ladder game!");
+            Console.WriteLine($"You are starting at position {playerPosition}.");
+
+
+        }
+    }
+}
