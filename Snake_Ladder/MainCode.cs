3a58e67d3d8187a713fed9d9a4edc75e683be125
Rohitpatil8951 <rohitpatil8951@gmail.com>
2024-06-11 10:38:35 +0530

    Play game - NOplay, Snake, Ladder done

--- Snake_Ladder/MainCode.cs
+++ Snake_Ladder/MainCode.cs
@@ -8,6 +8,32 @@
 
             Console.WriteLine("Welcome to the Snake and Ladder game!");
             Console.WriteLine($"You are starting at position {playerPosition}.");
+            Random random = new Random();
+
+            Console.WriteLine("\nPress Enter to roll the dice...");
+            Console.ReadLine();
+
+            int diceRoll = random.Next(1, 7);
+            Console.WriteLine($"You rolled a {diceRoll}.");
+
+            int option = random.Next(0, 3);
+
+            switch (option)
+            {
+                case 0: // No Play
+                    Console.WriteLine("No Play. You stay in the same position.");
+                    break;
+                case 1: // Ladder
+                    playerPosition += diceRoll;
+                    Console.WriteLine($"Ladder! You move ahead by {diceRoll} positions to {playerPosition}.");
+                    break;
+                case 2: // Snake
+                    playerPosition -= diceRoll;
+                    if (playerPosition < 0)
+                        playerPosition = 0; // Ensure position doesn't go negative
+                    Console.WriteLine($"Snake! You move back by {diceRoll} positions to {playerPosition}.");
+                    break;
+            }
 
 
         }
